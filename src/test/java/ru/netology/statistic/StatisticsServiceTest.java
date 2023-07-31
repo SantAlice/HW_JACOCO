6dd7ecf9521f46f71ab7b502eb48520b897ec226
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-07-31 22:30:39 +0300

    Modyfied test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
     @Test
+
     void findMax() {
         StatisticsService service = new StatisticsService();
 
